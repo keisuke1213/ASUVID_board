7ee19f9902ab4e2e578f56dbe526d4184f3c48fb
Keisuke1586 <fushanguifu@gmail.com>
2024-03-15 11:06:29 +0000

    ExpressionモデルのCRUD処理完了

--- app/controllers/expressions_controller.rb
+++ app/controllers/expressions_controller.rb
@@ -1,13 +1,45 @@
 class ExpressionsController < ApplicationController
   def new
+    @post = Post.find(params[:post_id])
+    @exp = Expression.new
   end
 
+ def create
+   @post = Post.find(params[:post_id])
+   @exp = current_user.expressions.new(expression_params)
+   @exp.post_id = @post.id
+   @exp.save
+   redirect_to post_path(@post)
+ end
   def index
+    @post = Post.find(params[:post_id])
   end
 
   def show
+    @exp = Expression.find(params[:id])
   end
 
   def edit
+    @exp = Expression.find(params[:id])
   end
+  
+  def update
+    @post = Post.find(params[:post_id])
+    @exp = Expression.find(params[:id])
+    @exp.update(expression_params)
+    redirect_to post_path(@post.id)
+  end
+  
+  def destroy
+    exp =  Expression.find(params[:id])
+    exp.destroy
+    redirect_to post_expressions_path
+  end
+  
+  private 
+  
+  def expression_params
+    params.require(:expression).permit(:title, :content, :message)
+  end
+  
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to posts_path
+    redirect_to post_path(@post.id)
   end
 
   def index
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
 
   def show
    @post = Post.find(params[:id])
+   @exp = Expression.new
   end
 
   def edit
@@ -32,7 +33,7 @@ class PostsController < ApplicationController
    post = Post.find(params[:id])
    post.destroy
    redirect_to posts_path
- end
+  end
   
   
    private
