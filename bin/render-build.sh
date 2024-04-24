422b6e1ed64c62671729d0dc33003f4ff9898b96
Keisuke1586 <fushanguifu@gmail.com>
2024-04-24 08:16:54 +0000

    render_build.shのrails db:migrateのコメントアウトを外す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-# bundle exec rake db:migrate  #この記述を追加
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:migrate  #この記述を追加
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
